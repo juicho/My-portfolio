b8549fd2b5b2a94eca0b8bee09e9b5f28d766f22
Jui chowdhury <herpower.juichowdhury@gmail.com>
2024-02-24 16:41:06 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".typing",{
+    strings: [ "Web Developer"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+
+var typed = new Typed(".typing-2",{
+    strings: ["Web Developer"],
+    typeSpeed:80,
+    backSpeed:80,
+});
+
+
+
+    
+
+
+   
